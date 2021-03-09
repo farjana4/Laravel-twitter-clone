a445b3a603c299992f77efb03a520fb239f2b263
farjana4 <mahjabin4bd@gmail.com>
2021-03-09 19:13:18 +0600

    Added three packages for development purpose, 1. debugbar, ide and code-style

---
+++ _ide_helper_models.php
@@ -0,0 +1,47 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ */
+	class User extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\tte
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|tte newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|tte newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|tte query()
+ */
+	class tte extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\tyu
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|tyu newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|tyu newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|tyu query()
+ */
+	class tyu extends \Eloquent {}
+}
+
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('email', 128)->unique();
+            $table->string('password', 128);
+            $table->string('username', 64)->unique()->nullable();
+            $table->string('first_name', 128)->unique();
+            $table->string('last_name', 128)->unique();
+            $table->string('phone_number', 128)->unique();
+            $table->string('bio')->unique()->nullable();
+            $table->string('location')->unique()->nullable();
+            $table->string('website')->unique()->nullable();
+            $table->date('date_of_birth')->unique()->nullable();
+            $table->timestamp('email_verified_at')->nullable()->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
